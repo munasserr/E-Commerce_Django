f986d1b8f1249e38d28d4738857614235d03dcaa
Muhammad Nasser <107954461+munasserr@users.noreply.github.com>
2023-01-04 20:23:57 +0200

    Update settings.py

--- djEcomBase/settings.py
+++ djEcomBase/settings.py
@@ -162,7 +162,7 @@ django_heroku.settings(locals(), staticfiles = False)
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
