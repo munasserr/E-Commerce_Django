38afe760c2ee6416518b5a4f97b1728eaf807fe1
Muhammad Nasser <107954461+munasserr@users.noreply.github.com>
2023-01-04 20:24:34 +0200

    Update urls.py

--- djEcomBase/urls.py
+++ djEcomBase/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('shop/' , include('shopping_app.urls')),
     # path('checkout/' , include('checkout_app.urls')),
     path('accounts/', include('allauth.urls')),
-] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT) + [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
